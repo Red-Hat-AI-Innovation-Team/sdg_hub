51eba02d0a497716c1135cf7d7073622bbf41ee8
shiv-sr <shiv.sr@ibm.com>
2024-07-29 21:16:44 +0000

    fix: minor bug fix in the conditional llm block
    Signed-off-by: shiv-sr <shiv.sr@ibm.com>

--- scripts/logger_config.py
+++ scripts/logger_config.py
@@ -9,7 +9,7 @@ from rich.logging import RichHandler
 def setup_logger(name):
     # Set up the logger
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.ERROR,
         format="%(message)s",
         datefmt="[%X]",
         handlers=[RichHandler()],
