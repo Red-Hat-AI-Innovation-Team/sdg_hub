146ea023a5246ebd938fb80123525c3f7d4e60f3
Shiv <shivchander.s30@gmail.com>
2024-12-03 12:02:44 -0800

    Major refactor #2 (#1)
    * refactor: ability to explicitly provide the flow config
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * feat: handle stop tokens (vllm fix) for generation
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * add annotation configs
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * refactor: better organization
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * feat: introducing registry - better handling of new blocks and prompts
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * refactor: update old templates to jinja
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * refactor: update file structures
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * refactor: removing some ilab related files
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * feat: add support for messages through LLMMessageBlock
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * sync: pushing current progress
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * syncing annotation benchmark experiments
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * refactor files
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    
    * cleaning up
    
    Signed-off-by: shiv <shivchander.s30@gmail.com>
    
    * Adding example annotation usecase
    
    * :sparkles: added a first draft of a readme
    
    * refactor: updates to data annotation section of readme
    
    * merge with main
    
    Signed-off-by: shiv <shivchander.s30@gmail.com>
    
    ---------
    
    Signed-off-by: shiv <shiv.sr@ibm.com>
    Signed-off-by: shiv <shivchander.s30@gmail.com>
    Co-authored-by: shiv <shiv.sr@ibm.com>

--- src/instructlab/sdg/logger_config.py
+++ src/instructlab/sdg/logger_config.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # Standard
+import os
 import logging
 
 # Third Party
@@ -8,8 +9,9 @@ from rich.logging import RichHandler
 
 def setup_logger(name):
     # Set up the logger
+    log_level = os.getenv("LOG_LEVEL", "INFO")
     logging.basicConfig(
-        level=logging.INFO,
+        level=log_level,
         format="%(message)s",
         datefmt="[%X]",
         handlers=[RichHandler()],
