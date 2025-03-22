272332e134ddb3b4d313c8f8fe148129ad5e05ad
Abhishek B <abhi1092@gmail.com>
2025-03-22 16:54:21 +0000

    adding script for running sdg
    Signed-off-by: Abhishek B <abhi1092@gmail.com>

--- run_sdg.sh
+++ run_sdg.sh
@@ -1,16 +1,19 @@
 # Get dataset size and save into variable
-dataset_size=$(wc -l /new_data/knowledge_rh/quality/base_datasets/quality_seed_example_chunked.jsonl | awk '{print $1}')
+dataset_path="/new_data/knowledge_rh/quality/base_datasets/entigraph_raw_dataset_percent_10.0_subset_uid_first_24.jsonl"
+dataset_size=$(wc -l $dataset_path | awk '{print $1}')
 number_of_processes=8
+echo "Dataset size: $dataset_size"
+
 port=8000
 for i in {0..7}; do
     dataset_start_index=$((i * dataset_size / number_of_processes))
     dataset_end_index=$((dataset_start_index + dataset_size / number_of_processes))
-    python scripts/generate.py --ds_path  /new_data/knowledge_rh/quality/base_datasets/quality_seed_example_chunked.jsonl \
+    python scripts/generate.py --ds_path $dataset_path \
         --bs 2 --num_workers 30 \
-        --save_path /new_data/knowledge_rh/quality/knowledge1.25_extractive_phi4/gen.jsonl \
-        --flow /home/lab/abhi/SDG-Research-Upstream/src/instructlab/sdg/flows/generation/knowledge/synth_knowledge1.5_phi4.yaml \
+        --save_path /new_data/knowledge_rh/quality/entigraph_knowledge1.0_phi4_first_24_n_5/gen/gen.jsonl \
+        --flow /home/lab/abhi/SDG-Research-Upstream/src/instructlab/sdg/flows/generation/knowledge/synth_knowledge_phi4.yaml \
         --endpoint http://localhost:$port/v1 \
-        --checkpoint_dir /new_data/knowledge_rh/quality/knowledge1.25_extractive_phi4/data_checkpoints \
+        --checkpoint_dir /new_data/knowledge_rh/quality/entigraph_knowledge1.0_phi4_first_24_n_5/data_checkpoints \
         --save_freq 1000 \
         --dataset_start_index $dataset_start_index \
         --dataset_end_index $dataset_end_index > run_sdg_$i.log 2>&1 &
