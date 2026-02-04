71cce4d04ceeaf296970e6c2f3e745398ebc0478
Mihir Athale <athale.m@northeastern.edu>
2026-02-04 13:56:18 -0500

    fix import sort

--- src/sdg_hub/core/blocks/transform/__init__.py
+++ src/sdg_hub/core/blocks/transform/__init__.py
@@ -10,8 +10,8 @@ from .duplicate_columns import DuplicateColumnsBlock
 from .index_based_mapper import IndexBasedMapperBlock
 from .json_structure_block import JSONStructureBlock
 from .melt_columns import MeltColumnsBlock
-from .row_multiplier import RowMultiplierBlock
 from .rename_columns import RenameColumnsBlock
+from .row_multiplier import RowMultiplierBlock
 from .text_concat import TextConcatBlock
 from .uniform_col_val_setter import UniformColumnValueSetter
 
